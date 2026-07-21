c18c2b164f72e322121fb3670600cf281a50734b
UnluckyLearns <252227031+UnluckyLearns@users.noreply.github.com>
2026-07-21 18:06:31 +0100

    Fixed A Bug In the Pawn Movement

--- Intermediate/Chess/pieces.py
+++ Intermediate/Chess/pieces.py
@@ -10,6 +10,7 @@ class Pieces():
         self.load = pygame.image.load(rf"C:\Users\User\Desktop\test\Chess\Chess_Pieces\{color}\{piece_name}.png")
     
     def get_pos(self,board):
+        print("here2")
         self.poss_moves = []
         for r,c in self.direction:
             row = self.row + r
@@ -28,35 +29,45 @@ class Pieces():
 
 class Pawns(Pieces):
     def __init__(self, color,col ,row,):
+        self.counter = 0
         super().__init__(color,col ,row,"Pawn")
     
-    def get_pos(self,board):
+    def get_pos(self, board):
         self.poss_moves = []
-        for mov in range(1,3): 
-            if  self.color == "White":
-                if self.col + mov  <= 7:
-                    if self.row + 1 <= 7:
-                        if board[self.col+1][self.row + 1].piece:
-                            if board[self.col+1][self.row + 1].piece.color =="Black":
-                                self.poss_moves.append((self.row + 1 ,self.col+1))
-                    if  board[self.col+1][self.row - 1].piece:
-                        if board[self.col+1][self.row - 1].piece.color =="Black":
-                         self.poss_moves.append((self.row - 1 ,self.col+1))
-                    if  board[self.col+mov][self.row].piece:
-                            return self.poss_moves
-                    self.poss_moves.append((self.row,self.col+mov))
-            elif self.color == "Black":
-                if   0 < self.col - mov:
-                    if self.row + 1 <= 7:
-                        if  board[self.col-1][self.row + 1].piece:
-                            if board[self.col-1][self.row + 1].piece.color =="White":
-                                self.poss_moves.append((self.row + 1 ,self.col-1))
-                    if  board[self.col-1][self.row - 1].piece:
-                        if board[self.col-1][self.row - 1].piece.color =="White":
-                            self.poss_moves.append((self.row - 1 ,self.col-1))
-                    if  board[self.col-mov][self.row].piece:
-                            return self.poss_moves
-                    self.poss_moves.append((self.row,self.col-mov))
+        if self.color == "White":
+            if self.col + 1 <= 7:
+                if self.row + 1 <= 7:
+                    if board[self.col + 1][self.row + 1].piece:
+                        if board[self.col + 1][self.row + 1].piece.color == "Black":
+                            self.poss_moves.append((self.row + 1, self.col + 1))
+                if self.row - 1 >= 0:
+                    if board[self.col + 1][self.row - 1].piece:
+                        if board[self.col + 1][self.row - 1].piece.color == "Black":
+                            self.poss_moves.append((self.row - 1, self.col + 1))
+                if not board[self.col + 1][self.row].piece:
+                    self.poss_moves.append((self.row, self.col + 1))
+                    if self.counter == 0:
+                        self.counter = 1
+                        if self.col + 2 <= 7:
+                            if not board[self.col + 2][self.row].piece:
+                                self.poss_moves.append((self.row, self.col + 2))
+        elif self.color == "Black":
+            if self.col - 1 >= 0:
+                if self.row + 1 <= 7:
+                    if board[self.col - 1][self.row + 1].piece:
+                        if board[self.col - 1][self.row + 1].piece.color == "White":
+                            self.poss_moves.append((self.row + 1, self.col - 1))
+                if self.row - 1 >= 0:
+                    if board[self.col - 1][self.row - 1].piece:
+                        if board[self.col - 1][self.row - 1].piece.color == "White":
+                            self.poss_moves.append((self.row - 1, self.col - 1))
+                if not board[self.col - 1][self.row].piece:
+                    self.poss_moves.append((self.row, self.col - 1))
+                    if self.counter == 0:
+                        self.counter = 1
+                        if self.col - 2 >= 0:
+                            if not board[self.col - 2][self.row].piece:
+                                self.poss_moves.append((self.row, self.col - 2))
         return self.poss_moves
 
 class Rook(Pieces):
